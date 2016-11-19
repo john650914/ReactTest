ef434c2433d6eb2eb0f126a9589f30906a1e80ad
john650914 <john650914@gmail.com>
2016-11-19 20:10:00 +0800

    新增header content footer

--- website1/components/app.js
+++ website1/components/app.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 class App extends React.Component {
 	render() {
-		return (<div className="app">app</div>);
+		return (
+			<div className="app">
+				<Header />
+				<Content />
+				<Footer />
+			</div>
+		);
 	}
 }
 
