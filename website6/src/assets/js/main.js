9de90194ced22f81a42d996c5117aa380d73e3d0
John Chang <johnchang@gamania.com>
2022-02-08 10:48:03 +0800

    just change indent

--- website6/src/assets/js/main.js
+++ website6/src/assets/js/main.js
@@ -63,17 +63,17 @@ class TodoApp extends React.Component {
 	}
 
 	render() {
-	return (
-		<div>
-			<h3>TODO</h3>
-			<TodoList items={this.state.items} />
-			<form onSubmit={this.handleSubmit}>
-				<label htmlFor="new-todo">What needs to be done?</label><br/>
-				<input id="new-todo" onChange={this.handleChange} value={this.state.text}/>
-				<button>Add #{this.state.items.length + 1}</button>
-			</form>
-		</div>
-	);
+		return (
+			<div>
+				<h3>TODO</h3>
+				<TodoList items={this.state.items} />
+				<form onSubmit={this.handleSubmit}>
+					<label htmlFor="new-todo">What needs to be done?</label><br/>
+					<input id="new-todo" onChange={this.handleChange} value={this.state.text}/>
+					<button>Add #{this.state.items.length + 1}</button>
+				</form>
+			</div>
+		);
 	}
 	handleChange(e) {
 		this.setState({ text: e.target.value });
